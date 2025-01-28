266022f3999c1b7f72a2db532bc69cdf45501e1e
Popoola Abdulhafeez <hafeezolayinka2005@gmail.com>
2025-01-28 21:20:02 +0100

    Added the add notes Modal

--- frontend/notes-app/src/pages/Home/AddEditNotes.jsx
+++ frontend/notes-app/src/pages/Home/AddEditNotes.jsx
@@ -1,28 +1,91 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import TagInput from '../../components/inputs/TagInput'
+import { MdClose } from 'react-icons/md'
+import Modal from 'react-modal' // Add this import
 
-const AddEditNotes = () => {
+Modal.setAppElement('#root') // Add this line
+
+const AddEditNotes = ({ noteData, type, onClose }) => {
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+    const [tags, setTags] = useState([])
+
+    useEffect(() => {
+        if (type === 'edit' && noteData) {
+            setTitle(noteData.title || '')
+            setContent(noteData.content || '')
+            setTags(noteData.tags || [])
+        }
+    }, [noteData, type])
+
+    const [error, setError] = useState(null)
+
+    // Add Note
+    const addNewNote = async () => {
+
+    }
+
+    // Edit Note
+    const editNote = async () => {
+
+    }
+
+    const handleAddNote = () => {
+        if (!title) {
+            setError('Please enter the title')
+            return
+        }
+
+        if (!content) {
+            setError("Please enter the content")
+            return;
+        }
+
+        setError('')
+
+        if (type === 'edit') {
+            editNote()
+        } else {
+            addNewNote()
+        }
+    }
     return (
-        <div>
+        <div className='relative'>
+            <button className="w-10 h-10 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-slate-50" onClick={onClose}>
+                <MdClose className='text-xl text-slate-400' />
+            </button>
+
             <div className="flex flex-col gap-2">
                 <label className='input-label'>TITLE</label>
-                <input type="text" className='text-2xl text-slate-950 outline-none' placeholder='Go To Gym At 5' />
+                <input
+                    type="text"
+                    className='text-2xl text-slate-950 outline-none'
+                    placeholder='Go To Gym At 5'
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
             </div>
 
             <div className="flex flex-col gap-2 mt-4">
                 <label className="input-label">CONTENT</label>
                 <textarea
                     type='text'
-                    className='text-sm text-slate-950 outline-nonebg-slate-50 p-2 rounded'
+                    className='text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded'
                     placeholder='Content'
                     rows={10}
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
                 />
             </div>
 
             <div className="mt-3">
                 <label className="input-label">TAGS</label>
+                <TagInput tags={tags} setTags={setTags} />
             </div>
 
-            <button className="btn-primary font-medium mt-5 p-3" onClick={() => { }}>
+            {error && <p className='text-red-500 text-xs pt-4'>{error}</p>}
+
+            <button className="btn-primary font-medium mt-5 p-3" onClick={handleAddNote}>
                 ADD
             </button>
         </div>
