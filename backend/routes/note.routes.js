14fa28c0fefd24623b96f4c59a8f5ea0eef65156
Popoola Abdulhafeez <hafeezolayinka2005@gmail.com>
2025-02-04 22:31:38 +0100

    Refactor routes and improve error handling; update .gitignore for node_modules

--- backend/routes/note.routes.js
+++ backend/routes/note.routes.js
@@ -10,11 +10,13 @@ const {
 const { authenticateToken } = require('../utilities');
 const router = express.Router();
 
-router.post('/add-note', authenticateToken, addNote);
-router.put('/edit-note/:noteId', authenticateToken, editNote);
-router.get('/get-all-notes/', authenticateToken, getAllNote);
-router.delete('/delete-note/:noteId', authenticateToken, deleteNote);
-router.put('/update-note-pinned/:noteId', authenticateToken, updateIsPinned);
-router.get('/search-notes/', authenticateToken, searchNotes);
+router.use(authenticateToken)
+
+router.post('/add-note', addNote);
+router.put('/edit-note/:noteId', editNote);
+router.get('/get-all-notes/', getAllNote);
+router.delete('/delete-note/:noteId', deleteNote);
+router.put('/update-note-pinned/:noteId', updateIsPinned);
+router.get('/search-notes/', searchNotes);
 
 module.exports = router;
